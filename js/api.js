9979144453028dcfbdeae294a407b5fac0c7105d
fbFideles <felipebreno106@gmail.com>
2020-06-30 20:56:40 -0300

    feat: adding create a task

---
+++ js/api.js
@@ -0,0 +1,24 @@
+const apiGetData = async () => {
+  try {
+    const response = await axios.get('https://floating-shore-71040.herokuapp.com/tasks')  
+    
+    return response
+  } 
+  catch (error) {
+    return error
+  } 
+} 
+
+const apiPostData = async (titleData, descriptionData) => {
+  try {
+    const response = await axios.post('https://floating-shore-71040.herokuapp.com/tasks', {
+      title: titleData,
+      description: descriptionData
+    })  
+    
+    return response
+  } 
+  catch (error) {
+    return error
+  } 
+}
--- js/index.js
+++ js/index.js
@@ -2,13 +2,36 @@ const addTaskButton = document.getElementById('create-task')
 const listTask = document.getElementById('list-task')
 
 const modal = document.getElementById('create-task-modal')
+const modalInput = document.getElementById('modalInput')
+const modalTextarea = document.getElementById('modalTextArea')
+
+const modalSubmitButton = document.getElementById('submitButton')
+const modalCloseButton = document.getElementById('closeButton')
 
 addTaskButton.onclick = () => {
   modal.style.display = 'block'
 }
 
-apiData().then(data => {
-  data.forEach(element => {
+modalSubmitButton.onclick = () => {
+  apiPostData(modalInput.value, modalTextarea.value)
+
+  modalInput.value = ''
+  modalTextarea.value = ''
+
+  modal.style.display = 'none'
+}
+
+modalCloseButton.onclick = () => {
+  modalInput.value = ''
+  modalTextarea.value = ''
+
+  modal.style.display = 'none'
+}
+
+apiGetData().then(response => {
+  response.data.reverse()
+
+  response.data.forEach(element => {
     const assembledTask = assembleTask(element.title, element.description)
 
     listTask.appendChild(assembledTask)
